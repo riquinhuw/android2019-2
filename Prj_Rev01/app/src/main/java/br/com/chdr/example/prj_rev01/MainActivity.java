788e5c920fe2798bd3f8921b276ef51c997e7a8c
Carlos Henrique <riquinhuw_lass@hotmail.com>
2019-09-20 08:33:47 -0300

    PrJ_Rev01 - Criado Telas de acrescimo e adição dos botões

--- Prj_Rev01/app/src/main/java/br/com/chdr/example/prj_rev01/MainActivity.java
+++ Prj_Rev01/app/src/main/java/br/com/chdr/example/prj_rev01/MainActivity.java
@@ -2,12 +2,27 @@ package br.com.chdr.example.prj_rev01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_descontoo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_descontoo = (Button) findViewById(R.id.btn_desconto);
+
+        btn_descontoo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
     }
 }
