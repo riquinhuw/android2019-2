151947a985c79a9639acd0fc80ba9fc9b3cfa404
Carlos Ribeiro <riquinhuw_lass@hotmail.com>
2019-09-19 23:07:16 -0300

    Update 1

---
+++ Prj_Rev01/app/src/main/java/br/com/chdr/example/prj_rev01/CalculoActivity.java
@@ -0,0 +1,13 @@
+package br.com.chdr.example.prj_rev01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalculoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculo);
+    }
+}
